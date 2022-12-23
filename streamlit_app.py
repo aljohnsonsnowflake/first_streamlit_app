284a62b370003a636ef7d4d68c4c74a947681dcc
aljohnsonsnowflake <121304221+aljohnsonsnowflake@users.noreply.github.com>
2022-12-23 10:48:55 -0500

    Update streamlit_app.py
    Users can select which fruit they want to add

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,4 @@
 import streamlit
-import pandas
-
-#using pandas to read csv file
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
@@ -14,5 +9,14 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-#displays the data pulled from csv file
+import pandas
+#using pandas to read csv file
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#to get the names of the fruit rather than its associated number
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+#Lets users pick fruits they want to includ
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+#displays the data pulled from csv file as a table
 streamlit.dataframe(my_fruit_list)
