c2eeddb7834d8d3e8d03eadcaed9347e5d55fae2
aljohnsonsnowflake <121304221+aljohnsonsnowflake@users.noreply.github.com>
2022-12-23 10:56:55 -0500

    Update streamlit_app.py
    filter the csv table to display just the fruits selected

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,10 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Lets users pick fruits they want to includ
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-#Pre-populate the user's selection with Avocado and Strawberries 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+#Pre-populate the user's selection with Avocado and Strawberries & save it in the variable fruits_selected
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 
+#pulls the rows in our csv file based on the fruits_selected & save it in the variable fruits_to_show
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 #displays the data pulled from csv file as a table
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
